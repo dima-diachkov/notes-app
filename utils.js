597cacf292f56d41377377cc93e08968fced632b
dima-diachkov <diachkov.dima@gmail.com>
2023-07-26 09:29:07 +0200

    add notes data

--- utils.js
+++ utils.js
@@ -1,17 +1,17 @@
 export function formatTime(date) {
     const options = {
-      year: 'numeric',
-      month: 'numeric',
-      day: 'numeric',
-      hour: '2-digit', // Use 2-digit format for hours (24-hour format)
-      minute: '2-digit',
-      hour12: false, // Use 24-hour format
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: '2-digit', // Use 2-digit format for hours (24-hour format)
+        minute: '2-digit',
+        hour12: false, // Use 24-hour format
     };
     return date.toLocaleString('en-US', options).replace(',', '');
-  }
-  
-  export function getDatesFromNoteContent(content) {
+}
+
+export function getDatesFromNoteContent(content) {
     const regex = /(\d{1,2}\/\d{1,2}\/\d{4})/g;
     const matches = content.match(regex);
     return matches ? matches : [];
-  }
+}
